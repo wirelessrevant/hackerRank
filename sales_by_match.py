344009f1a227b437803a083486dad9e17ace12be
wirelessrevant <wirelessrevant@gmail.com>
2022-12-19 23:01:37 -0500

    Add files via upload

---
+++ sales_by_match.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    count = 0
+    table = {}
+    for ele in ar:
+        if ele in table:
+            count+=1
+            table.pop(ele)
+        else:
+            table[ele] = 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
